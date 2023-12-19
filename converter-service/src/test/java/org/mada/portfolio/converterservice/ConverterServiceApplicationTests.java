095fa6461c010fc1f3e320c264d4084670c8e819
Mariusflores <mada060@student.kristiania.no>
2023-12-19 14:25:13 +0100

    inital commit + created a first controller

---
+++ converter-service/src/test/java/org/mada/portfolio/converterservice/ConverterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mada.portfolio.converterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
