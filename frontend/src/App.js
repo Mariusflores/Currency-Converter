15b7dfed1ac1d039d2c163fccc46d1fcea236413
Mariusflores <mada060@student.kristiania.no>
2023-12-22 13:43:50 +0100

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,23 @@ import './App.css';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Alert, Card, Container, Form} from 'react-bootstrap';
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
 import currencyDictionary from "./currencyCodes";
 import axios from 'axios'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExchangeAlt } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
     const [fromCurrency, setFromCurrency] = useState('USD');
     const [toCurrency, setToCurrency] = useState('USD');
     const [amount, setAmount] = useState(0);
-    const [convertedAmount, setConvertedAmount] = useState(0);
+    // eslint-disable-next-line
     const [showResult, setShowResult] = useState(false);
 
     const [alertFromCurrency, setAlertFromCurrency] = useState('---');
     const [alertToCurrency, setAlertToCurrency] = useState('---');
-    const [alertConvertedAmount, setAlertConvertedAmount] = useState('---');
+    const [alertConvertedAmount, setAlertConvertedAmount] = useState(0);
+    const [alertAmount, setAlertAmount] = useState(0);
 
     async function handleSubmit() {
         try {
@@ -30,12 +33,12 @@ function App() {
             )
 
             const {conversionResult, fromCode, toCode } = response.data;
-            setConvertedAmount(conversionResult);
 
             // Update the state values used in the Alert
             setAlertFromCurrency(fromCode);
             setAlertToCurrency(toCode);
-            setAlertConvertedAmount(conversionResult);
+            setAlertConvertedAmount(conversionResult.toFixed(2));
+            setAlertAmount(amount)
 
             setShowResult(true);
         } catch (error) {
@@ -43,41 +46,90 @@ function App() {
         }
     }
 
+    // Function to handle the swap of "From" and "To" currencies
+    const handleSwapCurrencies = () => {
+        setFromCurrency(toCurrency);
+        setToCurrency(fromCurrency);
+    };
+
+    const initializeCurrencies = () => {
+        const storedFromCurrency = localStorage.getItem('fromCurrency');
+        const storedToCurrency = localStorage.getItem('toCurrency');
+        const storedAmount = localStorage.getItem('amount');
+
+        if (storedFromCurrency && storedToCurrency) {
+            setFromCurrency(storedFromCurrency);
+            setToCurrency(storedToCurrency);
+            setAmount(storedAmount)
+        }
+    };
+
+    // Function to handle changes to currencies and store in localStorage
+    const handleCurrencyChange = (value, setterFunction) => {
+        console.log('Setter Function:', setterFunction.name);
+
+        setterFunction(value);
+
+        if (setterFunction === setFromCurrency) {
+            console.log('Storing in localStorage: fromCurrency');
+            localStorage.setItem('fromCurrency', value);
+        } else if (setterFunction === setToCurrency) {
+            console.log('Storing in localStorage: toCurrency');
+            localStorage.setItem('toCurrency', value);
+        }else if (setterFunction === setAmount){
+            localStorage.setItem('amount', value);
+        }
+
+    };
+
+
+
+
+
+    // Call initializeCurrencies when the component mounts
+    useEffect(() => {
+        initializeCurrencies();
+    }, []); // Empty dependency array ensures this effect runs once on mount
+
+
     return (
         <div className="App" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <Form className={'mb-2'}>
-                <Card bg={'dark'} style={{ width: '50rem', height: '25rem' }}>
-                    <Card.Header style={{ color: 'white' }}>Currency Converter</Card.Header>
+            <Form>
+                <Card bg={"dark"} style={{ width: '70rem', height: '35rem', alignItems: "center" }}>
+                    <Card.Header style={{ color: 'white', padding: '1.75rem' }}>Currency Converter</Card.Header>
                     <Card.Body style={{ display: 'flex' }}>
                         <Container>
                             <Form.Text style={{ color: 'white' }}>Amount</Form.Text>
                             <Form.Control
                                 type={'number'}
                                 value={amount}
-                                onChange={(e) => setAmount(e.target.value)}
+                                onChange={(e) => handleCurrencyChange(e.target.value, setAmount)}
                             />
                         </Container>
                         <Container>
                             <Form.Text style={{ color: 'white' }}>From</Form.Text>
-                            <Form.Select value={fromCurrency} onChange={(e) => setFromCurrency(e.target.value)}>
+                            <Form.Select value={fromCurrency} onChange={(e) => handleCurrencyChange(e.target.value, setFromCurrency)}>
                                 {Object.keys(currencyDictionary).map((code) => (
                                     <option key={code} value={code}>{code} ({currencyDictionary[code]})</option>
                                 ))}
                             </Form.Select>
                         </Container>
+                        <Button variant={"dark"} onClick={handleSwapCurrencies} style={{ marginTop: '1rem', height: '3rem'}}>
+                            <FontAwesomeIcon icon={faExchangeAlt}/>
+                        </Button>
                         <Container>
                             <Form.Text style={{ color: 'white' }}>To</Form.Text>
-                            <Form.Select value={toCurrency} onChange={(e) => setToCurrency(e.target.value)}>
+                            <Form.Select value={toCurrency} onChange={(e) => handleCurrencyChange(e.target.value, setToCurrency)}>
                                 {Object.keys(currencyDictionary).map((code) => (
                                     <option key={code} value={code}>{code} ({currencyDictionary[code]})</option>
                                 ))}
                             </Form.Select>
                         </Container>
                     </Card.Body>
-                    <Button onClick={handleSubmit}>Convert</Button>
+                    <Button variant={"secondary"} className={"btn-lg"}  onClick={handleSubmit}>Convert</Button>
                     {showResult && (
-                        <Alert variant="success" className="mt-2">
-                            {amount} {alertFromCurrency} = {alertConvertedAmount} {alertToCurrency}
+                        <Alert variant="secondary" className="mt-2">
+                            {alertAmount} {alertFromCurrency} = {alertConvertedAmount} {alertToCurrency}
                         </Alert>
                     )}
                 </Card>
